9d68bea517bdfc5bac20b5bbaed96713780a333c
Prabhat <prabhatbhasme@gmail.com>
2024-09-06 10:29:02 +0530

    first commit

---
+++ src/components/hero.jsx
@@ -0,0 +1,33 @@
+const HeroSection = () => {
+  return (
+    <main className="hero container">
+      <div className="hero-content">
+        <h1>YOUR FEET DESERVE THE BEST </h1>
+        <p>
+          YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR
+          SHOES.YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR
+          SHOES.
+        </p>
+        <div className="hero-btn">
+          <button class="button-30">Shop Now</button>
+          <button className="secondary-btn">Category</button>
+        </div>
+        <div className="shopping">
+          <p>Also Available On</p>
+          <div className="brand-icons">
+            <a href="https://www.flipkart.com/nike-run-swift-3-running-shoes-men/p/itma0cfc57c26db1?pid=SHOGTHZ5QFHSKZMN&lid=LSTSHOGTHZ5QFHSKZMNSUEBEI&marketplace=FLIPKART&q=nike+red+shoes&store=osp%2Fcil%2F1cu&srno=s_1_2&otracker=AS_QueryStore_OrganicAutoSuggest_1_8_na_na_na&otracker1=AS_QueryStore_OrganicAutoSuggest_1_8_na_na_na&fm=search-autosuggest&iid=7b410414-7184-4aa7-ab3f-54d4dc762642.SHOGTHZ5QFHSKZMN.SEARCH&ppt=sp&ppn=sp&ssid=jbgyb7t9cg0000001724666837493&qH=1a8ef6b0fda83a00">
+              <img src="/images/flipkart.png" alt="" />
+            </a>
+            <a href="https://www.amazon.in/Nike-University-Running-Shoes-13-5-847473-616/dp/B0192EW1Z6/ref=sr_1_3?dib=eyJ2IjoiMSJ9.BO1FzJ5JkwCnYESFeDJx8DJwyIqda1E_yGkDQzzEaROtFn6KOwZBavmZ5rek4STlftXEWmtww6yjqtVI9C-yCYiwyCrUREH1EE0hJT7qjbORg_EZ62RHVubzUFKUYqbZGnBK4c9FNdb3BzZ_1ZUJzgpjB5rarXdqIt2SIDtxUMyhyZL7ZXzCbwqc9aACgcH1fuHoZalGBhmBz0cKqxa_vkeLX5lyL1VSZlDZlDGqpCff6oS12x5-ghmHpqlgxw83Hhr4_3wPsK5Ugmqjp5ggwOxTx3xBuyF_zh_A-3fqAtQ.f1MbkyEJ3GIelnTHtVUOqDpdtsehvqhVfbujtbnWfxw&dib_tag=se&keywords=nike+shoes+for+men+red&qid=1724666808&sr=8-3">
+              <img src="/images/amazon.png" alt="" />
+            </a>
+          </div>
+        </div>
+      </div>
+      <div className="hero-image">
+        <img src="/images/shoe_image.png" alt="kaka" />
+      </div>
+    </main>
+  );
+};
+export default HeroSection;
---
+++ src/components/navigation.jsx
@@ -0,0 +1,37 @@
+import { useAuth0 } from "@auth0/auth0-react";
+
+const Navigation = () => {
+  const { loginWithRedirect ,isAuthenticated ,logout,user} = useAuth0();
+
+
+  return (
+    <nav className="nav">
+      <div className="logo">
+        <img src="/images/brand_logo.png" alt="kaka" />
+      </div>
+      <ul>
+        <li href="#">Menu</li>
+        <li href="#">Location</li>
+        <li href="#">About</li>
+        <li href="#">Contact</li>
+      </ul>
+      {
+        isAuthenticated &&<p>Welcome {user.name}</p>
+      }
+      {isAuthenticated ? (
+        <button
+          onClick={() =>
+            logout({ logoutParams: { returnTo: window.location.origin } })
+          }
+        >
+          Log Out
+        </button>
+      ) : (
+        <button className="login" onClick={() => loginWithRedirect()}>
+          login
+        </button>
+      )}
+    </nav>
+  );
+};
+export default Navigation;
